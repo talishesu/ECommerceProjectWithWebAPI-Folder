50cd95824582897a3cf0fc3a7e272c464977ea04
Talish <talishesu@code.edu.az>
2021-12-15 10:37:12 +0400

    15.12.2021 API Config

--- ECommerceProjectWithWebAPI/Models/Entities/Product.cs
+++ ECommerceProjectWithWebAPI/Models/Entities/Product.cs
@@ -10,6 +10,7 @@ namespace ECommerceProjectWithWebAPI.Models.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public int BrandId { get; set; }
+        public double Price { get; set; }
         public virtual Brand Brand { get; set; }
         public virtual ICollection<ProductImage> Images { get; set; }
         public virtual ICollection<ProductCategoryItem> CategoryItems { get; set; }
